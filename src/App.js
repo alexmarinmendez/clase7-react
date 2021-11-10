0f6c5943d1616a5fd5faf20f940571093aec88a2
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-11-10 09:29:39 -0500

    access to customFetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import customFetch from './utils/customFetch';
+
 function App() {
+  const [datos, setDatos] = useState([]);
+  const [error, setError] = useState({
+    isError: false,
+    errorMessage: ""
+  });
+
+  //componentDidMount
+  useEffect(() => {
+    customFetch()
+    .then(result => setDatos(result))
+    .catch(err => setError({
+      isError: true,
+      errorMessage: err
+    }))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Coder Simple App</h1>
+      <h2>This is a simple app for the React Course</h2>
+      <p>We are enjoying learning React!</p>
+      <hr />
+      <ol>
+        {
+          error.isError
+          ? <p>Hubo un error</p>
+          : datos.map((dato) => <li key={dato.id}>{dato.name}</li>)
+        }
+      </ol>
+      <hr />
+      &copy; 2021. Made with ❤ by the Coder Team.
+    </>
   );
 }
 
